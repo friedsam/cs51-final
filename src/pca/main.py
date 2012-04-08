5715cfae8eaed2c99c2a6655e6302d1f4e8c3a43
Yo Moon <moon@college.harvard.edu>
2012-04-08 19:56:19 -0400

    HRT Beta 0.1.0 second commit with pickle files ready

---
+++ src/pca/main.py
@@ -0,0 +1,9 @@
+import matrix 
+import pickle
+from numpy import * 
+
+#This is the main script that deals with PCA.
+
+#Processing Step:
+#Should get lists from the pickle files and make them into numpy arrays 
+
