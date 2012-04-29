40e22574c255a2745c6f1ab5b12b219477e08896
wchamber <wchamber@college.harvard.edu>
2012-04-29 17:30:59 -0300

    Commenting simplekmeans, fixing 'fromscratch' function

--- src/simplekmeans.py
+++ src/simplekmeans.py
@@ -6,26 +6,25 @@ from kmeans import *
 
 # remember to transpose!
 
-# takes a clusters object and a filename and makes .jpg's
 def kmeansClustersJpgs(clusters, filename):
+# takes a clusters object and a filename and makes .jpg's
     for j in range(len(clusters)):
         name = string.rstrip(filename,".pkl")
         name = name + "clust" + str(j) + "of" + str(len(clusters)-1) + ".jpg"
         montage.montage(clusters[j], open(name, "w"))
 
-# filename should be a pickle in format of datapoints
-# i.e parray is 1000 x 196 array
 def simplekmeans(filename, k):
+# filename should be a pickle in format of colmat
     parray = pickle.load(open(filename))
     (clusters, centroids) = makeClusters(parray, k)
     kmeansClustersJpgs(clusters, filename)
 
-# simplekmeans from test-1k.pkl format
 def simplekfromscratch(filename, k):
+# simplekmeans from test-1k.pkl format
     pkl = pickle.load(open(filename))
     leng = len(pkl)
     w = [0] * leng
     for k in range(leng):
         w[k] = array(pkl[k][0]).ravel()
-    (clusters, centroids) = makeClusters(array( w), k)
+    (clusters, centroids) = makeClusters(array( w).T, k)
     kmeansClustersJpgs(clusters, filename)
